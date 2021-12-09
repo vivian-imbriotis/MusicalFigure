8ff7efad3653f68955c09a8607c3370b56cd195e
Vivian Imbriotis <vivian.imbriotis@gmail.com>
2021-12-09 15:32:31 +1100

    got LEDs working

--- lightshow.py
+++ lightshow.py
@@ -2,7 +2,7 @@ import board
 import neopixel
 from av_handling import Musician
 
-from time import sleep
+from time import sleep, time
 
 import numpy as np
 
@@ -17,7 +17,13 @@ pixels = neopixel.NeoPixel(pixel_pin, n_led, brightness=0.2,
 
 musician = Musician("jinglebells.mp3")
 
-f = np.linspace(0,1000,n_led)
+f = np.linspace(0,700,n_led)
 
 musician.start_playback()
 
+while True:
+    t = time()
+    cols = musician.get_colors_for_frequencies(f,t=t)
+    cols = (cols * 255).astype(int)
+    pixels[:] = cols
+    sleep(0.05)
